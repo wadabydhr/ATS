7f3798c1e0890559ee76a40577f5d9311f518b22
Hiroki Wada <hiroki.wada@bydhr.com.br>
2025-06-26 14:48:38 -0300

    update header and footer content11

--- components/menu.py
+++ components/menu.py
@@ -2,6 +2,6 @@ from nicegui import ui
 
 def render_menu(classes=''):
     with ui.row().classes(f'gap-6 {classes}'):
-        ui.link('Dashboard', '/dashboard').classes('text-white text-sm hover:underline')
-        ui.link('Settings', '/settings').classes('text-white text-sm hover:underline')
-        ui.link('Logout', '/logout').classes('text-white text-sm hover:underline')
+        ui.link('GERAR RELATÓRIOS', '/dashboard').classes('text-white text-sm hover:underline')
+        ui.link('CONFIGURAÇÕES', '/settings').classes('text-white text-sm hover:underline')
+        ui.link('LOGOUT', '/logout').classes('text-white text-sm hover:underline')
