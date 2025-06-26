24b91e9f303cdf89bee9fc77bc4ffd5aef17e3a3
Hiroki Wada <hiroki.wada@bydhr.com.br>
2025-06-26 14:09:29 -0300

    update header and footer content8

--- components/header.py
+++ components/header.py
@@ -4,6 +4,6 @@ from components.menu import render_menu
 def render_header(user):
     with ui.row().classes('w-full bg-primary text-white p-4 items-center'):
         ui.label('BEYOND HR ATS').classes('text-lg font-bold')
-        render_menu()  # Insert the menu inside the header
+        render_menu(classes='ml-auto')  # Insert the menu inside the header
         if user and user.get("name"):
             ui.label(f'Logado como {user["name"]}').classes('ml-auto')
